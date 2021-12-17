2697a319f2200efffe03a68b489f83b98a00fed9
Vikram Singh <vikramsingh@MB-M-VIKRAMS.local>
2021-12-17 14:57:25 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'angularPage1', component: Page1Component },
+  { path: 'angularPage2', component: Page2Component },
+  { path: '**', component: EmptyRouteComponent }
+];
 
 @NgModule({
+  declarations: [EmptyRouteComponent],
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
 })
 export class AppRoutingModule { }
